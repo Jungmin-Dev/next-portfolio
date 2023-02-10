419dae0380283138bde14f5744e652ea146b67b3
JungminKim <kimjungmin__@naver.com>
2023-02-10 15:22:49 +0900

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -15,7 +15,7 @@ export default function Footer(){
                     </Link>
 
                     <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2023 —
-                        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@식빵이 주인</a>
+                        <a href="https://open.kakao.com/o/gdnECF3e" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@식빵이 주인</a>
                     </p>
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                         <a className="text-gray-500">
